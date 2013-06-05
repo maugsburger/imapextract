bf2060e8369dd247e58ee3486828e17b26dc3ac5
Moritz Augsburger <moritz@augsburger.name>
2013-06-05 10:35:51 +0000

    implement idle

--- imapextract.pl
+++ imapextract.pl
@@ -2,54 +2,82 @@
 
 use warnings;
 use strict;
+use v5.10;
 
 use Email::MIME 1.901;
 use IO::Socket::SSL;
 use Mail::IMAPClient;
 use POSIX qw/ strftime /;
 use Config::IniFiles;
+use Data::Dumper;
 
+my $exit = 0;
 my $cfg = Config::IniFiles->new( -file => "conf.ini" );
 
 my $imap = Mail::IMAPClient->new(
-    Debug      => 1,
+    Debug       => 1,
     User        => $cfg->val( "server", 'user'),
     Password    => $cfg->val( "server", 'pass'),
     Uid         => 1,
-    Peek        => 1,  # don't set \Seen flag
     Server      => $cfg->val( "server", 'host'),
     Port        => $cfg->val( "server", 'port'),
     Ssl         => $cfg->val( "server", 'ssl'),
 );
 
+die "$0: login: $@" if $@;
 
 $imap->select("INBOX")
     or die "$0: select INBOX: ", $imap->LastError, "\n";
 
-my $today = strftime "%Y%m%d", localtime $^T;
-my @messages = $imap->search(SUBJECT => $today);
-die "$0: search: $@" if defined $@;
+while($imap->IsConnected && !$exit){
+    my @messages = $imap->messages();
+    die "$0: search: $@" if $@;
 
-foreach my $id (@messages) {
-    die "$0: funky ID ($id)" unless $id =~ /\A\d+\z/;
+    $SIG{'INT'} = $SIG{'TERM'} = \&use_next_exit;
 
-    my $str = $imap->message_string($id)
-        or die "$0: message_string: $@";
+    foreach my $id (@messages) {
+        sleep 100;
+        die "$0: funky ID ($id)" unless $id =~ /\A\d+\z/;
 
-    my $n = 1;
-    Email::MIME->new($str)->walk_parts(sub {
-            my($part) = @_;
-            return unless $part->content_type =~ /\bname="([^"]+)"/;  # " grr...
+        my $str = $imap->message_string($id)
+            or die "$0: message_string: $@";
 
-            my $name = "./$today-$id-" . $n++ . "-$1";
-            print "$0: writing $name...\n";
-            open my $fh, ">", $name
-                or die "$0: open $name: $!";
-            print $fh $part->content_type =~ m!^text/!
-            ? $part->body_str
-            : $part->body
-                or die "$0: print $name: $!";
-            close $fh
-                or warn "$0: close $name: $!";
-        });
+        my $n = 1;
+        Email::MIME->new($str)->walk_parts(sub {
+                my($part) = @_;
+                return unless $part->content_type =~ /\bname="([^"]+)"/;  # " grr...
+
+                my $name = "./" . time . $n++ . "-$1";
+                print "$0: writing $name...\n";
+                open my $fh, ">", $name
+                    or die "$0: open $name: $!";
+                print $fh $part->content_type =~ m!^text/!
+                ? $part->body_str
+                : $part->body
+                    or die "$0: print $name: $!";
+                close $fh
+                    or warn "$0: close $name: $!";
+            });
+        $imap->delete_message($id);
+    }
+
+    $imap->expunge("INBOX") or die "Could not expunge: $@\n";
+
+    $SIG{'INT'} = $SIG{'TERM'} = 'DEFAULT';
+
+    last if $exit;
+    if ( $cfg->val( "server", 'idle')) {
+        my $tag = $imap->idle or warn "idle failed: $@\n";
+        $imap->idle_data($cfg->val( "server", 'wait')) or warn "idle_data error: $@\n";
+        $imap->done($tag) or warn "Error from done: $@\n";
+    } else {
+        sleep $cfg->val( "server", 'wait');
+    }
+
+}
+
+$imap->disconnect or warn "Could not logout: $@\n";
+
+sub use_next_exit() {
+    $exit = 1;
 }
